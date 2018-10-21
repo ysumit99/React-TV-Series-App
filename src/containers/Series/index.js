9c39fd9eb00e25589f8f47ed884366a993b2ef8d
ysumit99 <ysumit99@gmail.com>
2018-10-21 12:25:40 +0530

    Using SeriesList component in Series container

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import SeriesList from "../../components/SeriesList";
 
 class Series extends Component {
   state = {
@@ -13,7 +14,12 @@ class Series extends Component {
   }
 
   render() {
-    return <div>The length of series array = {this.state.series.length}</div>;
+    return (
+      <div>
+        The length of series array = {this.state.series.length}
+        <SeriesList list={this.state.series} />
+      </div>
+    );
   }
 }
 
