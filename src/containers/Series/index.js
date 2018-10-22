95b62da55c8b10b285a622e701e9b609f85a5975
ysumit99 <ysumit99@gmail.com>
2018-10-22 09:00:38 +0530

    Adding Form input field to make search dynamic

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -7,16 +7,25 @@ class Series extends Component {
   };
 
   //used for initial rendering of App Component
-  componentDidMount() {
-    fetch("http://api.tvmaze.com/search/shows?q=vikings")
+  // componentDidMount() {
+  //   fetch("http://api.tvmaze.com/search/shows?q=vikings")
+  //     .then(response => response.json())
+  //     .then(json => this.setState({ series: json }));
+  // }
+
+  onSeriesInputChange = e => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
       .then(response => response.json())
       .then(json => this.setState({ series: json }));
-  }
+  };
 
   render() {
     return (
       <div>
         The length of series array = {this.state.series.length}
+        <div>
+          <input type="text" onChange={this.onSeriesInputChange} />
+        </div>
         <SeriesList list={this.state.series} />
       </div>
     );
