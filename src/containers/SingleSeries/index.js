44ba4eb1b8522a35fa8d39c86b971e0c87cf7a62
ysumit99 <ysumit99@gmail.com>
2018-10-22 13:17:37 +0530

    final commit

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import Loader from "../../components/Loader";
 
 class SingleSeries extends Component {
+  state = {
+    show: null
+  };
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    fetch(`http://api.tvmaze.com/shows/${id}?embed-episodes`)
+      .then(response => response.json())
+      .then(json => this.setState({ show: json }));
+  }
   render() {
-    console.log(this.props);
+    const { show } = this.state;
+    console.log(show);
     return (
       <div>
-        <p>Single Series - the show id will be {this.props.match.params.id}</p>
+        {show === null && <Loader />}
+        {show !== null && (
+          <div>
+            <p>{show.name}</p>
+            <p>Premiered - {show.premiered}</p>
+            <p>Rating - {show.rating.average}</p>
+            {/* <p>Episodes - {show.webChannel.weight}</p> */}
+            <p>
+              <img alt="Show" src={show.image.medium} />
+            </p>
+          </div>
+        )}
       </div>
     );
   }
